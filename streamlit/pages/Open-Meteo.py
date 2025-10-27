fa41e6a69c172c91d816bb10c5b04428e87cc3e1
oLi-ECL <skylabs4580@gmail.com>
2025-10-27 00:02:11 -0600

    Update Open-Meteo.py

--- streamlit/pages/Open-Meteo.py
+++ streamlit/pages/Open-Meteo.py
@@ -64,7 +64,7 @@ if err:
 
 st.dataframe(df, use_container_width=True)
 
-fig = px.line(df, x='time', y='temperature', title="Today's Temp (Denver)")
+fig = px.scatter(df, x='time', y='temperature', title="Today's Temp (Denver)")
 st.plotly_chart(fig, use_container_width=True)
 
 # If auto-refresh is ON, wait and rerun the app
